5ea41eee394600c7befdfd07222e43ee691f94c1
MrYaz <LehaHotPlay@yandex.ru>
2026-03-25 12:45:41 +0500

    Изменён BaseController.java
    Теперь контроллер отвечает за загрузку VBOX в Главную менюку BorderPane

--- app/src/main/java/mryazik/github/io/BaseController.java
+++ app/src/main/java/mryazik/github/io/BaseController.java
@@ -1,25 +1,44 @@
 package mryazik.github.io;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
+import java.net.URL;
 
 public class BaseController
 {
-//    private App appFX;
-
-    @FXML
-    private Button stats_button;
+    private App appFx;
+    private VBox rootLayout;
 
     @FXML
     public void initialize()
     {
-        stats_button.setOnAction(event -> {
-            System.out.println("lox");
-        });
+
     }
-//
-//    public void setAppFX(App appFX) {
-//        this.appFX = appFX;
-//    }
+
+    public void setAppFX(App appFX) {
+        this.appFx = appFX;
+    }
+
+    public void defaultLoad()
+    {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+
+            URL fxmlLocation = getClass().getResource("/maket/MainVBOX.fxml");
+            loader.setLocation(fxmlLocation);
+
+            if (fxmlLocation == null) {
+                throw new RuntimeException("Файл FXML не найден! Проверьте путь.");
+            }
+
+            rootLayout = loader.load();
+            appFx.rootLayout.setCenter(rootLayout);
+        } catch (IOException e)
+        {
+            System.out.println(e.toString());
+        }
+    }
 }
