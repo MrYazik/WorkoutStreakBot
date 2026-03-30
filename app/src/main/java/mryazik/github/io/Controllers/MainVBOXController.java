90c5bc7c30c24fa32ab76b7b2ba05dbf485e755b
MrYaz <LehaHotPlay@yandex.ru>
2026-03-30 10:44:45 +0500

    Добавлены изменения в MainVBOXController
    (Данный контроллер отвечает за работу кнопок "Статистика ""Мои тренеровки")
    
    Теперь кнопка "Мои тренеровки" загружают новый VBOX заместо старого и загружает контроллер TrainersVBOX

--- app/src/main/java/mryazik/github/io/Controllers/MainVBOXController.java
+++ app/src/main/java/mryazik/github/io/Controllers/MainVBOXController.java
@@ -1,4 +1,57 @@
 package mryazik.github.io.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import mryazik.github.io.App;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// Отвечает за то чтоб управлять кнопками MainVBOX:
+
 public class MainVBOXController {
+    @FXML private Button stats_button;
+    @FXML private Button trainers_button;
+    private App app;
+    VBox rootLayout;
+
+    private Logger logger = Logger.getLogger(MainVBOXController.class.getName());
+
+    public void initialize()
+    {
+        stats_button.setOnAction(event -> {
+            System.out.println("Log: button \"Статистика\"");
+        });
+        trainers_button.setOnAction(event -> {
+            try {
+                System.out.println("Log: button \"Мои тренеровки\"");
+
+                FXMLLoader loader = new FXMLLoader();
+                URL fxmlLocation = getClass().getResource("/maket/TrainersVBOX.fxml");
+                loader.setLocation(fxmlLocation);
+
+                rootLayout = loader.load();
+
+//                 Загружаем ещё один контроллер, меню моих тренеровок
+                TrainersVBOX controller = loader.getController();
+                controller.setApp(app);
+
+                controller.initList(this); // добавляем кнопки по тренеровкам
+
+                app.rootLayout.setCenter(rootLayout);
+            }
+            catch (IOException e) {
+                logger.log(Level.SEVERE, "Не удалось сменить rootLayout", e);
+            }
+        });
+    }
+
+    public void setApp(App app)
+    {
+        this.app = app;
+    }
 }
