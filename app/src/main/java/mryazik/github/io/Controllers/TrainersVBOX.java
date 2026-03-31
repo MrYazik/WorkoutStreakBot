bff8147d262c23c08f5070ec48f6de3afe24a2da
MrYaz <LehaHotPlay@yandex.ru>
2026-03-31 12:25:20 +0500

    В контролере, который отвечает за список тренеровок была добавленна реализация кнопки назада
    Данная кнопка реализована при помощи новой загрузке прошлого FXML файла, загрузке от туда контролера, передача ссылки на класс App и вызова необходимых методов

--- app/src/main/java/mryazik/github/io/Controllers/TrainersVBOX.java
+++ app/src/main/java/mryazik/github/io/Controllers/TrainersVBOX.java
@@ -27,10 +27,14 @@ public class TrainersVBOX {
     @FXML
     public VBox list_vbox;
 
-    public void initList(MainVBOXController backController)
+    public void initList()
     {
         Excercise trainers = Trainers.getFromFile();
         Map<String, Object> listTrainers = Objects.requireNonNull(trainers.getAllTrainings(), "TrainersVBOX.java: список тренеровок пустой");
+
+        // Загружаем VBOX определённой тренеровки
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/maket/SelectedTrainersVBOX.fxml"));
 
         listTrainers.forEach((key, object) -> {
             Button btn = new Button(key);
@@ -39,9 +43,27 @@ public class TrainersVBOX {
 
             list_vbox.getChildren().add(
                     btn
-            );
+            ); // добавляем кнопку
+
+            // Добавляем функционал
+            btn.setOnAction(event -> {
+                try {
+                    VBox rootLayout;
+                    rootLayout = loader.load();
+
+                    SelectTrainersVBOX controller = loader.getController();
+                    controller.setBackController(this);
+                    controller.initVBOX(key, (Map<String, Object>) object);
+
+                    app.rootLayout.setCenter(rootLayout);
+                } catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            });
 
             logger.log(Level.INFO, "Created new button", key);
+
         });
     }
 
@@ -50,5 +72,26 @@ public class TrainersVBOX {
     public void setApp(App app)
     {
         this.app = app;
+    }
+
+    @FXML Button back;
+    public void initialize()
+    {
+        back.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/maket/MainMenu.fxml"));
+                loader.load();
+
+                BaseController controller = loader.getController();
+                controller.setAppFX(app);
+                controller.defaultLoad();
+
+                // app.rootLayout.setCenter(rootLayout);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        });
     }
 }
