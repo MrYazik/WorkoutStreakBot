0443088cf1cdfed203fd8acae80e69fd660fb935
MrYaz <LehaHotPlay@yandex.ru>
2026-04-07 16:56:42 +0500

    Добавленны изменения в контроллер меню, отвечающий за настройку упражнений (SettingsOneEx.java)
    Добавленно изменение файла тренеровок, теперь всё что в полях изменяется, то изменяется в файле по нажатии кнопки "Готово"
    
    В действия обеих кнопок: Готово и Отмена добавленна загрузка предыдущего меню

--- app/src/main/java/mryazik/github/io/Controllers/SettingsOneEx.java
+++ app/src/main/java/mryazik/github/io/Controllers/SettingsOneEx.java
@@ -10,9 +10,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import mryazik.github.io.App;
 import mryazik.github.io.Classes.Trainers;
+import mryazik.github.io.Classes.WorkImage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -43,14 +46,32 @@ public class SettingsOneEx {
 
     String name_trainers;
     String name_ex;
+    int pics_id;
+
+    // id предыдущей добавленной картинки
+    AtomicInteger oldImageId = new AtomicInteger(0);
 
     public void setBackcontroller(SelectTrainersVBOX backController)
     {
         this.backController = backController;
     }
-    public void initOneEx(String name_trainers, String name_ex, int count_ap, int count_comp)
+    public void initOneEx(String name_trainers, String name_ex, int count_ap, int count_comp, int pics_id)
     {
         this.name_trainers = name_trainers;
+        this.pics_id = pics_id;
+
+        // Загружаем картинку
+        if (pics_id != 0)
+        {
+            try {
+                Image image1 = new Image(WorkImage.getImage(pics_id).toUri().toString());
+                this.image.setImage(image1);
+
+                oldImageId.set(pics_id);
+            } catch (IllegalArgumentException e) { // если нет такого файла
+                oldImageId.set(0);
+            }
+        } // Потом добавить загрузку стандартной картинки из resources
 
         ex_name.setText(name_ex);
         this.name_ex = name_ex;
@@ -60,33 +81,65 @@ public class SettingsOneEx {
 
     public void initialize()
     {
+        AtomicInteger count_ap = new AtomicInteger(0);
+        AtomicInteger count_comp = new AtomicInteger(0);
+
+
         paste_image.setOnAction(action -> {
+            // Если значение уже есть, то удаляем файл по id:
+
+            if (oldImageId.get() != 0)
+            {
+                WorkImage.removeImage(oldImageId.get());
+            }
+
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("Установите изображение для упражнения");
 
             File selectedFile = fileChooser.showOpenDialog(backController.backController.app.primaryStage);
-            Image image = new Image(selectedFile.toURI().toString());
+            oldImageId.set(WorkImage.loadImage(selectedFile));
 
+
+            Image image = new Image(WorkImage.getImage(oldImageId.get()).toUri().toString());
             this.image.setImage(image);
         });
 
         // Кнопка готово
         done.setOnAction(event -> {
-            AtomicInteger count_ap = new AtomicInteger(0);
-            AtomicInteger count_comp = new AtomicInteger(0);
-
             // собираем новую инфу
             try {
                 count_ap.set(Integer.parseInt(this.count_ap.getText()));
                 count_comp.set(Integer.parseInt(this.count_comp.getText()));
             } catch (NumberFormatException e)
             {
-                logger.log(Level.WARNING, "Строка введённоая пользователем - это строка", e);
+                logger.log(Level.WARNING, "Строка введённая пользователем - это строка", e);
             }
 
-            Trainers.addEx(this.name_trainers, this.name_ex,
-                    Trainers.sampleEx(count_ap.get(), count_comp.get(), 1)
-                    );
+            Map<String, Object> newMap = Trainers.sampleEx(count_ap.get(), count_comp.get(), oldImageId.get());
+            Map<String, Object> editTrainersMap = Trainers.addEx(this.name_trainers, this.name_ex, newMap);
+
+            // Возвращаемся назад
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/maket/SelectedTrainersVBOX.fxml"));
+
+                VBox rootLayout = loader.load();
+
+                SelectTrainersVBOX controller = loader.getController();
+                controller.setBackController(backController.backController);
+
+                // Получим упражнения из тренировки
+                editTrainersMap.forEach((key, object) -> {
+                    if (key.equals(backController.name_trainers_text))
+                    {
+                        controller.initVBOX(backController.name_trainers_text, (Map<String, Object>)object);
+                    }
+                });
+
+                backController.backController.app.rootLayout.setCenter(rootLayout);
+            }catch (IOException e) {
+                logger.log(Level.SEVERE, "Не удалось загрузить из SettingsOneEx обратно в SelectedTrainersVBOX.fxml");
+            }
         });
 
         back.setOnAction(event ->
@@ -99,7 +152,18 @@ public class SettingsOneEx {
 
                 SelectTrainersVBOX controller = loader.getController();
                 controller.setBackController(backController.backController);
-                controller.initVBOX(backController.name_trainers_text, backController.ex_map);
+
+                // собираем новую инфу
+                try {
+                    count_ap.set(Integer.parseInt(this.count_ap.getText()));
+                    count_comp.set(Integer.parseInt(this.count_comp.getText()));
+                } catch (NumberFormatException e)
+                {
+                    logger.log(Level.WARNING, "Строка введённоая пользователем - это строка", e);
+                }
+
+                Map<String, Object> newMap = Trainers.sampleEx(count_ap.get(), count_comp.get(), oldImageId.get());
+                controller.initVBOX(backController.name_trainers_text, newMap);
 
                 backController.backController.app.rootLayout.setCenter(rootLayout);
             } catch (IOException e)
