6aba5833e281677225de0cd0f4836418f5d4c368
MrYaz <LehaHotPlay@yandex.ru>
2026-04-13 12:14:00 +0500

    Был добавлен основной функцияонал контроллеру InTrainers.java
    - Инициализация -- отвечает за инициализацию списков упражнений тренеровки
    
    - Добавлен метод changeEx, который получает id упражнения в списке всех упражнений и в json объекте ищет это упражнение - подставляет информацию
    
    - В метод changeEx - было добаленна смена статуса упражнения и смена статуса всей тренеровки при необходимости
    
    - Метод backMenu - создан новый подход, на который возможно переведётся программа - из всех мест где нужно вернуться назад, будет загружаться backMenu

--- app/src/main/java/mryazik/github/io/Controllers/InTrainers.java
+++ app/src/main/java/mryazik/github/io/Controllers/InTrainers.java
@@ -1,13 +1,27 @@
 package mryazik.github.io.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import mryazik.github.io.Classes.WorkImage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class InTrainers {
     SelectTrainersVBOX backController;
+    Logger logger = Logger.getLogger(getClass().getName());
 
     // Поля FXML
     @FXML
@@ -24,9 +38,125 @@ public class InTrainers {
     Button done;
     @FXML
     Button back;
+    @FXML
+    Label count_co; // количество выполнений (текст)
 
     public void setBackcontroller(SelectTrainersVBOX backController)
     {
         this.backController = backController;
+    }
+
+    // Переменные необходимые объекту
+    // Мы модем позволить static, ведь архитектура ПО не предполагает однвременного запуска нескольких программ
+    static int max_ap = 1; // максимальное количество подходов
+    static double current_ap; // текущее количество выполненых подходов
+    static int current_ex; // index в list_ex упражнения
+    static double current_ex_proc; // index в list_ex упражнения
+
+
+    static List<String> list_ex = new ArrayList<>();
+    static Map<String, Object> ex;
+
+    public void init(String name_trainers, Map<String, Object> ex)
+    {
+        this.name_trainers.setText(name_trainers);
+        this.ex = ex;
+
+        ex.forEach((key, object) -> {
+                list_ex.add(key);
+        });
+
+        logger.log(Level.INFO, "Последний элемент при добавлении в список тренеровок:" + list_ex.getLast());
+
+        changeEx(current_ex); // по умолчанию там 0
+    }
+
+    public void changeEx(int current_ex_id)
+    {
+        String name_ex = list_ex.get(current_ex_id);
+        Map<String, Object> ex_info = (Map<String, Object>) ex.get(name_ex);
+
+        this.name_ex.setText(name_ex);
+
+        // Значение бара тренеровки
+        current_ex_proc += (double) 1 / list_ex.size();
+        proc_trainers.setProgress(current_ex_proc);
+
+        ex_info.forEach((key, object) -> {
+            if (key.equals("cout_apporachies")) // если количество подходов
+            {
+                max_ap = Integer.parseInt(object.toString());
+            } else if (key.equals("cout_complite"))
+            {
+                count_co.setText(object.toString());
+            } else if (key.equals("pics_id")) {
+                try {
+
+                    java.nio.file.Path path_to_image = WorkImage.getImage(Integer.parseInt(object.toString()));
+                    Image image = new Image(path_to_image.toUri().toString());
+                    image_ex.setImage(image);
+                } catch (IllegalArgumentException e)
+                {
+                    logger.log(Level.WARNING, "Нету изображения при загрузке упражнения");
+                    image_ex.setImage(WorkImage.getStandartImage());
+                }
+            }
+        });
+
+
+        // Значение progress bar-а по умолчанию
+        current_ap = 0;
+        comp_ap.setProgress(0);
+
+        // Реакция кнопок
+        done.setOnAction(event -> {
+            current_ap += Math.round((double) 1 / max_ap * 100.0) / 100.0;
+
+            if (current_ap <= 1)
+            {
+                logger.log(Level.INFO, "Количество выполненных подходов: " + current_ap);
+                comp_ap.setProgress(current_ap);
+
+                if (current_ap >= 1)
+                {
+                    done.setText("Выполнил упражнение");
+                }
+            } else
+            {
+                done.setText("Выполнил подход");
+                current_ex++;
+
+                if (current_ex > list_ex.size())
+                {
+                    backMenu();
+                } else
+                {
+                    changeEx(current_ex);
+                }
+            }
+        });
+    }
+
+    public void initialize()
+    {
+        back.setOnAction(event -> backMenu());
+    }
+
+    public void backMenu()
+    {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/maket/SelectedTrainersVBOX.fxml"));
+
+            VBox rootLayout = loader.load();
+
+            SelectTrainersVBOX controller = loader.getController();
+            controller.setBackController(backController.backController);
+            controller.initVBOX(backController.name_trainers_text, backController.ex_map);
+
+            backController.backController.app.rootLayout.setCenter(rootLayout);
+        } catch (IOException e)
+        {
+            logger.log(Level.WARNING, "Не удалось загрузить SelectedTrainersVBOX.fxml", e);
+        }
     }
 }
