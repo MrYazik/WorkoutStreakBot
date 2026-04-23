7e8ce315269d4f13c6bc733d6f0eedafce3a5f08
MrYaz <LehaHotPlay@yandex.ru>
2026-04-23 11:27:01 +0500

    Добавленны поля к которым можно обращаться в контроллер CreateEx.java

--- app/src/main/java/mryazik/github/io/Controllers/CreateEx.java
+++ app/src/main/java/mryazik/github/io/Controllers/CreateEx.java
@@ -1,4 +1,22 @@
 package mryazik.github.io.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
 public class CreateEx {
+    @FXML
+    TextField name_ex;
+    @FXML
+    TextField count_ap;
+    @FXML
+    TextField count_comp;
+
+    @FXML Button paste_impage;
+    @FXML Button done;
+    @FXML Button back;
+    @FXML ImageView image;
+
+
 }
